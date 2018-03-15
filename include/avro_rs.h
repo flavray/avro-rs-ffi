8b268638cd3338a6343a8b65c20ded8decb61ec9
Antonio Verardi <antonio@yelp.com>
2018-03-15 14:45:00 +0100

    Add header file

---
+++ include/avro_rs.h
@@ -0,0 +1,10 @@
+/* c bindings to the avro-rs library */
+
+#ifndef AVRO_RS_BINDINGS_H
+#define AVRO_RS_BINDINGS_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#endif /* AVRO_RS_BINDINGS_H */
